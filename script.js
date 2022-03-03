ee36b10e0560b43e53a109dbcd7e271af9d77a79
Joy Adigwe <joyadigwe7@gmail.com>
2022-03-03 14:34:09 -0600

    guidelines

---
+++ script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+       document.getElementById("empty").innerHTML = "Heads Up! This service includes basic lashes, eyeshadow, highlight, blush, and lipgloss or lipstick!";
+
+}
+
+function function2() {
+    document.getElementById("empty").addEeventListener("click", () => {
+     document.getElementById("empty").innerHTML = "hey";   
+    });
+}
+
+function myFunction2() {
+    document.getElementById("empty2").innerHTML = "Heads Up! This service includes dramatic lashes, eyeshadow with or without glitter, highlight, blush, and lipgloss or lipstick."
+}
+
+function myFunction3() {
+    document.getElementById("empty3").innerHTML = "Heads Up! If you come with your own makeup materials then the price will remain $30. If you would like me to provide the makeup materials, then the price will be $40. More information regarding makeup classes is available under the Guidelines section."
+}
